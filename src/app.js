c7c7a2214aeab74bed7362a36e1b1d2c1cff1bc0
Georg Preuss <preussg@gmail.com>
2019-11-21 10:38:02 +0000

    basic webpack setup

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style.scss'
+
+// console.log(process.env.API_KEY)
+
+const App = () => (
+  <h1>hello</h1>
+)
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
